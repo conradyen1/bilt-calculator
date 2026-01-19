30fac3ffaa77dccc8ecc6ca853e2ad2908585797
conrad <conradyen@conrads-MacBook-Pro.local>
2026-01-18 19:33:01 -0800

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "bilt-calculator",
+  base: "/bilt-calculator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
